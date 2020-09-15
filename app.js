44b12616fa38f9d3bd0dafd3e6ec2daa46909066
mahaking51 <mahaking144@gmail.com>
2020-09-16 00:47:27 +0530

    added auth and request.rest to test

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const jwt =require('jsonwebtoken')
+const jwt =require('jsonwebtoken');
+app.use(express.json());
 const userDet=[
     {
         username:'Maharajan',
@@ -17,10 +19,38 @@ const userDet=[
 ]
 
 
-app.get('/',function(req,res){
-
+app.get('/',auth,function(req,res){
+res.json(userDet.filter(det => det.username === req.user.name && det.password===req.user.pwd));
+})
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+    user={
+        name:username,
+        pwd:password
+    }
+    const accessToken=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET);
+    res.json({accessToken:accessToken})
 })
 
+
+function auth(req,res,next){
+const header = req.headers['authorization'];
+const tkn=header && header.split(' ')[1];
+if(tkn==null){
+return res.sendStatus(401)
+}
+jwt.verify(tkn,process.env.ACCESS_TOKEN_SECRET,function(err,user){
+    if(err){
+        res.sendStatus(403);
+    }
+    else{
+        req.user=user;
+        next();
+    }
+})
+
+}
 app.listen(3000,function() {
     console.log('server started on port 3000');
 })
